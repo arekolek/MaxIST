024ed97e0d46ca7d02fbc82bc8f9101b66b3bb19
Arek Olek <arkadiusz.olek@uj.edu.pl>
2014-08-02 20:34:38 +0200

    refactor

--- eval.cpp
+++ eval.cpp
@@ -6,6 +6,7 @@
 
 #include "debug.hpp"
 #include "dfs.hpp"
+#include "rdfs.hpp"
 #include "test_suite.hpp"
 #include "options.hpp"
 
@@ -14,6 +15,21 @@ boost::property<boost::edge_color_t,
 boost::adjacency_list<
   boost::hash_setS, boost::vecS, boost::undirectedS,
   boost::no_property, color>            typedef graph;
+
+template <class Graph>
+int eval(Graph const & T) {
+  int n = num_vertices(T);
+  int internal = 0;
+  int upper = n - 2;
+  auto vs = vertices(T);
+  for(auto vit = vs.first; vit != vs.second; ++vit)
+    internal += degree(*vit, T) > 1;
+
+  cout << n-internal << " " << internal << " " << upper
+    << " " << n << " " << (upper-internal)/(double)upper << endl;
+
+  return n-internal;
+}
 
 int main(int argc, char** argv){
   std::ios_base::sync_with_stdio(0);
@@ -30,18 +46,11 @@ int main(int argc, char** argv){
   for(auto G : suite) {
     auto T = dfs_tree(G);
 
-    int internal = 0;
-    int upper = n - 2;
-    auto vs = vertices(T);
-    for(auto vit = vs.first; vit != vs.second; ++vit)
-      internal += degree(*vit, T) > 1;
+    sum += eval(T);
 
-    cout << n-internal << " " << internal << " " << upper
-      << " " << n << " " << (upper-internal)/(double)upper << endl;
+    T = rdfs_tree(G);
 
-    sum += n-internal;
-
-    show("graph.dot", G, T);
+    //show("graph.dot", G, T);
   }
 
   cout << sum/(double)suite.size() << endl;
---
+++ rdfs.hpp
@@ -0,0 +1,19 @@
+// (C) 2014 Arek Olek
+
+#pragma once
+
+#include <stack>
+
+#include <boost/graph/adjacency_list.hpp>
+
+template <class Graph>
+Graph rdfs_tree(Graph& G) {
+  Graph T;
+
+
+  stack<unsigned> s;
+
+  //visit(r);
+
+  return T;
+}
