2f2225979492aad72d74ed0c75f49f107eb1ac6e
Arek Olek <arkadiusz.olek@uj.edu.pl>
2014-08-02 14:25:01 +0200

    eval

---
+++ dfs.hpp
@@ -0,0 +1,42 @@
+// (C) 2014 Arek Olek
+
+#pragma once
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/undirected_dfs.hpp>
+
+using boost::edge_color;
+using boost::on_tree_edge;
+
+template <class NewGraph, class Tag>
+struct graph_copier
+  : public boost::base_visitor<graph_copier<NewGraph, Tag> >
+{
+  typedef Tag event_filter;
+
+  graph_copier(NewGraph& graph) : new_g(graph) { }
+
+  template <class Edge, class Graph>
+  void operator()(Edge e, Graph& g) {
+    add_edge(source(e, g), target(e, g), new_g);
+  }
+private:
+  NewGraph& new_g;
+};
+
+template <class NewGraph, class Tag>
+inline graph_copier<NewGraph, Tag>
+copy_graph(NewGraph& g, Tag) {
+  return graph_copier<NewGraph, Tag>(g);
+}
+
+template <class Graph>
+Graph dfs_tree(Graph& G) {
+  Graph T;
+
+  undirected_dfs(G,
+    visitor(make_dfs_visitor(copy_graph(T, on_tree_edge())))
+      .edge_color_map(get(edge_color, G)));
+
+  return T;
+}
---
+++ eval.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+
+#include <boost/graph/adjacency_list.hpp>
+
+#include "debug.hpp"
+#include "dfs.hpp"
+#include "test_suite.hpp"
+#include "options.hpp"
+
+boost::property<boost::edge_color_t,
+  boost::default_color_type>            typedef color;
+boost::adjacency_list<
+  boost::hash_setS, boost::vecS, boost::undirectedS,
+  boost::no_property, color>            typedef graph;
+
+int main(int argc, char** argv){
+  std::ios_base::sync_with_stdio(0);
+
+  options opt(argc, argv);
+  int z = opt.get<int>("-z", 1);
+  int n = opt.get<int>("-n", 5);
+  float p = opt.get<float>("-p", 1);
+
+  test_suite<graph> suite(z, n, p);
+
+  int sum = 0;
+
+  for(auto G : suite) {
+    auto T = dfs_tree(G);
+
+    int internal = 0;
+    int upper = n - 2;
+    auto vs = vertices(T);
+    for(auto vit = vs.first; vit != vs.second; ++vit)
+      internal += degree(*vit, T) > 1;
+
+    cout << n-internal << " " << internal << " " << upper
+      << " " << n << " " << (upper-internal)/(double)upper << endl;
+
+    sum += n-internal;
+
+    show("graph.dot", G, T);
+  }
+
+  cout << sum/(double)suite.size() << endl;
+
+  return 0;
+}
