4e821b74ed6ebc86eca2dc1d4c4b34ab15ed763d
Arek Olek <arkadiusz.olek@uj.edu.pl>
2014-11-23 23:31:51 +0100

    generate random geometric graphs

--- util/graph.hpp
+++ util/graph.hpp
@@ -4,9 +4,17 @@
 
 #include <algorithm>
 #include <functional>
+#include <utility>
 #include <vector>
 
+#include <boost/graph/adjacency_list.hpp>
 #include <boost/iterator/counting_iterator.hpp>
+#include <boost/iterator/function_input_iterator.hpp>
+#include <boost/function_output_iterator.hpp>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Triangulation_vertex_base_with_info_2.h>
+#include <CGAL/Point_set_2.h>
 
 #include "range.hpp"
 
@@ -36,4 +44,33 @@ void add_edges_uniform(Graph& G, double p, Generator generator) {
     for(unsigned j = i + 1; j < n; ++j)
       if(trial())
         add_edge(i, j, G);
+}
+
+template<class Graph, class Generator>
+void add_random_geometric(Graph& G, double d, Generator generator) {
+  CGAL::Exact_predicates_inexact_constructions_kernel           typedef Kernel;
+  CGAL::Triangulation_vertex_base_with_info_2<unsigned, Kernel> typedef Vertex_struct;
+  CGAL::Triangulation_data_structure_2<Vertex_struct>           typedef Triangulation_struct;
+  CGAL::Point_set_2<Kernel, Triangulation_struct>               typedef Delaunay;
+  CGAL::Circle_2<Kernel>                                        typedef Circle;
+  Delaunay::Vertex_handle                                       typedef Vertex_handle;
+  Delaunay::Point                                               typedef Point;
+
+  // Choose random points in square [0, 1) x [0, 1)
+  std::uniform_real_distribution<> distribution(0, 1);
+  auto r = std::bind(distribution, generator);
+  // Generate index for every point
+  int n = boost::num_vertices(G), counter = 0;
+  std::function<std::pair<Point, int>()> f =
+    [&]() {return std::make_pair(Point(r(), r()), counter++); };
+  // Initialize triangulation with n points
+  Delaunay D(boost::make_function_input_iterator(f, 0),
+             boost::make_function_input_iterator(f, n));
+  // Query each point for other points lying in the circle
+  for(auto v : range(D.finite_vertices_begin(), D.finite_vertices_end()))
+    D.range_search(Circle(v.point(), d*d), boost::make_function_output_iterator(
+        [&](Vertex_handle u){
+          if(v.info() < u->info())
+            boost::add_edge(v.info(), u->info(), G);
+    }));
 }
