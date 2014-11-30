8a7d8230bb3c9fac127df842617d75f1a54f118f
Arek Olek <arkadiusz.olek@uj.edu.pl>
2014-11-30 20:21:13 +0100

    default values for option list

--- util/options.hpp
+++ util/options.hpp
@@ -28,20 +28,23 @@ namespace opt_util {
 
 class options {
 public:
-  template <class Result = std::string>
-  Result get(std::string name, Result def = Result()) {
+  unsigned find(std::string const& name) const {
     for(int i = 1; i < argc-1; ++i)
       if(strcmp(argv[i], name.c_str()) == 0)
-        return opt_util::convert<Result>(argv[i+1]);
-    return def;
+        return i + 1;
+    return 0;
+  }
+  template <class Result = std::string>
+  Result get(std::string name, Result def = Result()) {
+    return find(name) ? opt_util::convert<Result>(argv[find(name)]) : def;
   }
   template <class Result>
-  std::vector<Result> getList(std::string name) {
+  std::vector<Result> getList(std::string name, std::initializer_list<Result> def = {}) {
     std::vector<Result> result;
     std::stringstream text(get(name));
     for(std::string token; std::getline(text, token, ',');)
       result.push_back(opt_util::convert<Result>(token.c_str()));
-    return result;
+    return find(name) ? result : def;
   }
   options(int c, char** v) : argc(c), argv(v) { }
 private:
