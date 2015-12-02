d4bca0c957e6b4ec8ae5c071af81895489afae06
Arek Olek <arkadiusz.olek@uj.edu.pl>
2015-12-02 17:11:40 +0100

    modify gml2xml.py

--- gml2xml.py
+++ gml2xml.py
@@ -1,12 +1,14 @@
 
 import graph_tool.all as gt
 from sys import argv
+from re import findall
 
 if __name__ == '__main__':
   for f in argv[1:]:
-    name = f.split('--')[0]
     g = gt.GraphView(gt.load_graph(f), directed=False, skip_properties=True)
     gt.remove_parallel_edges(g)
     gt.remove_self_loops(g)
-    g.save('{}.xml'.format(name))
+    name = findall('[^/.]+', f)[-2].split('--')[0]
+    g.save('output/{}.xml'.format(name))
+    gt.graph_draw(g, output='output/{}.png'.format(name))
 
